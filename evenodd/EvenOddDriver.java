f92af91cb32ee9bab4c3dbba1ddf5ef41fecf5a9
dinesh-g1 <dineshgillalaa@gmail.com>
2024-12-30 21:14:21 +0530

    feat: multiple even and odd threads

--- evenodd/EvenOddDriver.java
+++ evenodd/EvenOddDriver.java
@@ -5,9 +5,13 @@ public class EvenOddDriver {
     public static int curr = 1;
     public static void main(String[] args) {
         Object lock = new Object();
-        Thread odd = new Thread(new OddWorker("odd", lock));
-        Thread even = new Thread(new EvenWorker("even", lock));
-        odd.start();
-        even.start();
+        Thread odd1 = new Thread(new OddWorker("odd1", lock));
+        Thread odd2 = new Thread(new OddWorker("odd2", lock));
+        Thread even1 = new Thread(new EvenWorker("even1", lock));
+        Thread even2 = new Thread(new EvenWorker("even2", lock));
+        odd1.start();
+        odd2.start();
+        even1.start();
+        even2.start();
     }
 }
